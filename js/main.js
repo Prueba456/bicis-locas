8bfa2106faa180befa9ff3d360cd14f03eb93b02
Betsi Loayza <betsi18_12@hotmail.com>
2017-03-03 12:41:00 -0500

    primer paso

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function validateForm(){
+	/* Escribe tú código aquí */
+var contras = document.getElementById("input-password");
+  if (contras.value == "password" || contras.value=="123456" || contras.value=="098754"){
+  alert("Elija otra contraseña");
+  }
+
+    var bici = document.getElementById("bici")
+    if(bici.value == null || bici.value == "") 
+
+
+
+
+}
