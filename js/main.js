f0c7a65b0ba0132a433f7334ba28bd2c9619ed6c
Betsi Loayza <betsi18_12@hotmail.com>
2017-03-03 19:08:45 -0500

    validacion terminada

--- js/main.js
+++ js/main.js
@@ -1,14 +1,42 @@
 function validateForm(){
 	/* Escribe tú código aquí */
-var contras = document.getElementById("input-password");
-  if (contras.value == "password" || contras.value=="123456" || contras.value=="098754"){
-  alert("Elija otra contraseña");
-  }
+var nombre = document.getElementById("name");
+var apellido = document.getElementById("lastname");
+var correo = document.getElementById("input-email");
+var contrasena = document.getElementById("input-password");
+var combo = document.getElementById("combo").selectedIndex;
 
-    var bici = document.getElementById("bici")
-    if(bici.value == null || bici.value == "") 
+if(nombre.value == "" || /[0-9]/.test(nombre.value)  ){
+  alert("Vuelva a escribir su nombre");
+}
 
+if(nombre.value.charAt(0).toUpperCase() != nombre.value.charAt(0) ){
+  alert("Escribir la primera letra de tu nombre en mayuscula");
+}
+if(apellido.value == "" || /[0-9]/.test(apellido.value)){
+  alert("Escriba su apellido");
+}
+if(apellido.value.charAt(0).toUpperCase() != apellido.value.charAt(0) ){
+    alert("Escribir la primera letra de tu apellido en mayuscula");
+}
 
+if(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,4}$/.test(correo.value)){
+
+}else if(correo.value == ""){
+  alert("Escribir correo");
+}else{
+  alert("La dirección de correo es incorrecta ");
+}
+if(contrasena.value == "password"|| contrasena.value == "123456" || contrasena.value == "098754"){
+  alert("Ingresa otra contraseña");
+}else if(/.{6,}/.test(contrasena.value)){
+
+}else{
+  alert("Ingresa una contraseña mayor de 6 digitos");
+}
+if( combo == null || combo == 0 ) {
+  alert('No hay opción seleccionada');
+}
 
 
 }
